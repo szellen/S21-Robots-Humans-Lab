2f57338081d00f65e24e78ddfab57f34be461bb5
jiajia Liang <jl9pg@virginia.edu>
2021-03-15 01:24:42 -0400

    add latest lab4 project and crg file

---
+++ Lab_Work/Lab4_pinTail/translations/translation_en_US.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Found</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Found</translation>
+        </message>
+        <message>
+            <source>Tell me a number between 0 and 9</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tell me a number between 0 and 9</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Tell me a number between 0 and 4</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Tell me a number between 0 and 4</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Pass me the pen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Pass me the pen</translation>
+        </message>
+        <message>
+            <source>Tell me a number between 0 and 9</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tell me a number between 0 and 9</translation>
+        </message>
+        <message>
+            <source>Found it!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Found it!</translation>
+        </message>
+        <message>
+            <source>found it</source>
+            <comment>Text</comment>
+            <translation type="obsolete">found it</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>found it. please give me the pen</source>
+            <comment>Text</comment>
+            <translation type="unfinished">found it. please give me the pen</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>found it. please give me the pen</source>
+            <comment>Text</comment>
+            <translation type="obsolete">found it. please give me the pen</translation>
+        </message>
+        <message>
+            <source>Not the number I'm looking for</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Not the number I'm looking for</translation>
+        </message>
+        <message>
+            <source>not the number</source>
+            <comment>Text</comment>
+            <translation type="obsolete">not the number</translation>
+        </message>
+        <message>
+            <source>no</source>
+            <comment>Text</comment>
+            <translation type="obsolete">no</translation>
+        </message>
+        <message>
+            <location filename="../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../Program Files (x86)/Softbank Robotics/Choregraphe Suite 2.8/translations/behavior_1/behavior.xar" line="0"/>
+            <source></source>
+            <comment>Text</comment>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Not the number</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Not the number</translation>
+        </message>
+    </context>
+</TS>
